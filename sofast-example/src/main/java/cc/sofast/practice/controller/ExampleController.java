e2ba98e05f8321cd052a1653dd30a69d4824c21e
Administrator <Administrator@qq.com>
2025-04-23 22:58:33 +0800

    feat(example): 添加 API 文档并配置相关依赖
    - 在 ExampleController 中添加 Swagger 注解，用于生成 API 文档
    - 在 application.yml 中配置 Springdoc 和 Knife4j 相关设置
    - 在 pom.xml 中添加 Knife4j 依赖，用于增强 API 文档功能

--- sofast-example/src/main/java/cc/sofast/practice/controller/ExampleController.java
+++ sofast-example/src/main/java/cc/sofast/practice/controller/ExampleController.java
@@ -2,7 +2,11 @@ package cc.sofast.practice.controller;
 
 import cc.sofast.biz.component.log.mapper.LogMapper;
 import cc.sofast.biz.component.rbac.mapper.SysMenuMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +22,9 @@ public class ExampleController {
 
     private final LogMapper logMapper;
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
+    @SecurityRequirement(name = HttpHeaders.AUTHORIZATION)
+    @Operation(summary = "描述1")
     public String hello() {
         return "hello";
     }
