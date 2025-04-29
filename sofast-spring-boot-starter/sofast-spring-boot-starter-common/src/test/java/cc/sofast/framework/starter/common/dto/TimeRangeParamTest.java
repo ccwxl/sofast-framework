07b31658f7ec2058d8e03dbfe472321d4633f805
ccwxl <siaron.wang@gmail.com>
2025-04-29 17:07:54 +0800

    build:移除 sofast-spring-boot-starter 子模块 - 删除了多个 sofast-spring-boot-starter 子模块的 pom.xml 文件 - 从 sofast-spring-boot-starter/pom.xml 中移除了相关子模块的引用 - 删除了 sofast-spring-boot-starter-test 和 sofast-spring-cloud-starter-feign 模块

--- sofast-spring-boot-starter/sofast-spring-boot-starter-common/src/test/java/cc/sofast/framework/starter/common/dto/TimeRangeParamTest.java
+++ sofast-spring-boot-starter/sofast-spring-boot-starter-common/src/test/java/cc/sofast/framework/starter/common/dto/TimeRangeParamTest.java
@@ -8,7 +8,6 @@ class TimeRangeParamTest {
     public void testParser() {
         String query = "https://teset.sofast.cc?registerDate=2025-04-21 11:00:00.123,2025-04-22 11:00:00.123";
         String query2 = "https://teset.sofast.cc?test=1&registerDate=2025-04-21 11:00:00.123,2025-04-22 11:00:00.123";
-        TimeRangeParam timeRangeParam = TimeRangeParam.parser(query);
 
     }
 }
