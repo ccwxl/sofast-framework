300d516cf9072bfb1d96ad9c7fb6dfd8435a69c6
ccwxl <siaron.wang@gmail.com>
2025-04-30 17:04:30 +0800

    refactor(sofast-spring-boot-starter-common): 优化 BeanSerializerTranslator 类的注释
    - 修改了循环嵌套相关的 TODO 注释，使其描述更加准确
    - 优化了代码结构，提高了可读性

--- sofast-spring-boot-starter/sofast-spring-boot-starter-common/src/main/java/cc/sofast/framework/starter/common/trans/translator/BeanSerializerTranslator.java
+++ sofast-spring-boot-starter/sofast-spring-boot-starter-common/src/main/java/cc/sofast/framework/starter/common/trans/translator/BeanSerializerTranslator.java
@@ -7,7 +7,7 @@ public class BeanSerializerTranslator extends SerializerTranslator<Object> {
 
     //TODO class ---> 标注@Trans注解的 filed cache
     //TODO class ---> ref 字段cache
-    //TODO 有可能循环嵌套,这种场景需要将次字段进行忽略
+    //TODO 有可能循环嵌套,避免深度遍历导致出现问题
 
     @Override
     public void serialize(Object value) {
