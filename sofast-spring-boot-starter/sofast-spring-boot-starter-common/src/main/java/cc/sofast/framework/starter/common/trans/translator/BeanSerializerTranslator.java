bbdd9e3655abf7832f38a928cfcf6f3249d4c9fa
ccwxl <siaron.wang@gmail.com>
2025-04-30 16:55:10 +0800

    refactor(sofast-spring-boot-starter-common): 添加循环引用处理TODO
    - 在 BeanSerializerTranslator 类中添加了处理循环引用的 TODO 注释
    - 此修改提醒开发者在序列化过程中需要注意并处理可能存在的循环引用问题

--- sofast-spring-boot-starter/sofast-spring-boot-starter-common/src/main/java/cc/sofast/framework/starter/common/trans/translator/BeanSerializerTranslator.java
+++ sofast-spring-boot-starter/sofast-spring-boot-starter-common/src/main/java/cc/sofast/framework/starter/common/trans/translator/BeanSerializerTranslator.java
@@ -7,6 +7,7 @@ public class BeanSerializerTranslator extends SerializerTranslator<Object> {
 
     //TODO class ---> 标注@Trans注解的 filed cache
     //TODO class ---> ref 字段cache
+    //TODO 有可能循环嵌套,这种场景需要将次字段进行忽略
 
     @Override
     public void serialize(Object value) {
