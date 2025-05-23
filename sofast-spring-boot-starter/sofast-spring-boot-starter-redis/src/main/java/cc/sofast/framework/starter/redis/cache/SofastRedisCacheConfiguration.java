a0741d2ded138301468cbe5fc6a3a942fc52cfd8
ccwxl <siaron.wang@gmail.com>
2025-05-23 10:49:55 +0800

    refactor(framework): 优化 Redis 缓存配置
    - 移除了未使用的导入语句，简化代码结构
    - 删除了不必要的注释和空行，提高代码可读性

--- sofast-spring-boot-starter/sofast-spring-boot-starter-redis/src/main/java/cc/sofast/framework/starter/redis/cache/SofastRedisCacheConfiguration.java
+++ sofast-spring-boot-starter/sofast-spring-boot-starter-redis/src/main/java/cc/sofast/framework/starter/redis/cache/SofastRedisCacheConfiguration.java
@@ -5,15 +5,11 @@ import cc.sofast.framework.starter.redis.codec.SofastCacheGenericJackson2JsonRed
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.interceptor.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Role;
 import org.springframework.data.redis.cache.BatchStrategies;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -24,7 +20,6 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.util.StringUtils;
 
 import java.util.Objects;
-import java.util.function.Supplier;
 
 /**
  * @author wxl
