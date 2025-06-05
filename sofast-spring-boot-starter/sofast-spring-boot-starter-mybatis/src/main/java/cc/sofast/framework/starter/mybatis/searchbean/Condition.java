3705869e5221d8da736c2100ac34fb948cb2ed76
ccwxl <siaron.wang@gmail.com>
2025-06-05 15:40:08 +0800

    feat(mybatis): 增加更多搜索条件枚举值
    - 新增了 likeLeft、in、likeRight 和 between 等搜索条件
    - 为每个条件添加了相应的注释说明
    -这些新增的条件将支持更复杂的查询需求

--- sofast-spring-boot-starter/sofast-spring-boot-starter-mybatis/src/main/java/cc/sofast/framework/starter/mybatis/searchbean/Condition.java
+++ sofast-spring-boot-starter/sofast-spring-boot-starter-mybatis/src/main/java/cc/sofast/framework/starter/mybatis/searchbean/Condition.java
@@ -10,16 +10,51 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Condition {
+    /**
+     * status=eq:ACTIVE
+     */
     eq("eq"),
+    /**
+     * status=ne:ACTIVE
+     */
     ne("ne"),
+    /**
+     * status=lt:ACTIVE
+     */
     lt("lt"),
+    /**
+     * status=le:ACTIVE
+     */
     le("le"),
+    /**
+     * status=gt:ACTIVE
+     */
     gt("gt"),
+    /**
+     * status=ge:ACTIVE
+     */
     ge("ge"),
+    /**
+     * status=like:ACTIVE
+     */
     like("like"),
+    /**
+     * status=ll:ACTIVE
+     */
     likeLeft("likeLeft"),
+    /**
+     * ?role=in:Manager,Engineer
+     */
     in("in"),
-    likeRight("likeRight");
+    /**
+     * ?role=lr:Manager,Engineer
+     */
+    likeRight("likeRight"),
+
+    /**
+     * ?role=bt:1,2
+     */
+    between("between");
 
     private final String expression;
 }
