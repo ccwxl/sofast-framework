62cd2b2ed258f8274d351ad3110f53aa7e155865
Administrator <Administrator@qq.com>
2025-06-17 22:45:32 +0800

    feat(beansearch): 优化字段条件过滤和排序条件处理
    - 添加逻辑删除字段条件过滤
    -优化字段条件过滤逻辑，支持动态添加到 BeanMeta
    - 改进排序条件处理，确保正确解析和添加排序字段

--- sofast-spring-boot-starter/sofast-spring-boot-starter-mybatis/src/main/java/cc/sofast/framework/starter/mybatis/beansearch/MyBeanSearcher.java
+++ sofast-spring-boot-starter/sofast-spring-boot-starter-mybatis/src/main/java/cc/sofast/framework/starter/mybatis/beansearch/MyBeanSearcher.java
@@ -24,15 +24,18 @@ public class MyBeanSearcher extends DefaultBeanSearcher {
             throw new SearchException("You must set a sqlExecutor before searching.");
         }
         BeanMeta<T> beanMeta = getMetaResolver().resolve(beanClass);
+        // 逻辑删除字段条件过滤
         if (beanMeta.getFieldMeta("deleted") == null) {
             FieldMeta fieldMeta = new FieldMeta(beanMeta, "deleted", null, new SqlSnippet(StrUtil.toUnderlineCase("deleted")), UUID.randomUUID().toString(),
                     true, new Class[]{}, DbType.UNKNOWN, Cluster.FALSE);
             beanMeta.addFieldMeta(fieldMeta);
         }
 
+        // 字段条件过滤 添加到BeanMeta
         for (Map.Entry<String, Object> searchParam : paraMap.entrySet()) {
             Object value = searchParam.getValue();
             String key = searchParam.getKey();
+            //where 条件
             if (value instanceof FieldParam fieldParam) {
                 FieldMeta fieldMeta = new FieldMeta(beanMeta, fieldParam.getName(), null, new SqlSnippet(StrUtil.toUnderlineCase(fieldParam.getName())), UUID.randomUUID().toString(),
                         true, new Class[]{}, DbType.UNKNOWN, Cluster.FALSE);
@@ -40,13 +43,14 @@ public class MyBeanSearcher extends DefaultBeanSearcher {
                     beanMeta.addFieldMeta(fieldMeta);
                 }
             }
+            // 排序条件
             if ("cn.zhxu.bs.param.OrderBy".equals(key)) {
                 if (value instanceof ArrayList<?> arrayList) {
                     for (Object o : arrayList) {
                         if (o instanceof OrderBy orderBy) {
                             FieldMeta fieldMeta = beanMeta.getFieldMeta(orderBy.getSort());
                             if (fieldMeta == null) {
-                                fieldMeta = new FieldMeta(beanMeta, orderBy.getSort(), null,  new SqlSnippet(StrUtil.toUnderlineCase(orderBy.getSort())), UUID.randomUUID().toString(),
+                                fieldMeta = new FieldMeta(beanMeta, orderBy.getSort(), null, new SqlSnippet(StrUtil.toUnderlineCase(orderBy.getSort())), UUID.randomUUID().toString(),
                                         true, new Class[]{}, DbType.UNKNOWN, Cluster.FALSE);
                                 beanMeta.addFieldMeta(fieldMeta);
                             }
