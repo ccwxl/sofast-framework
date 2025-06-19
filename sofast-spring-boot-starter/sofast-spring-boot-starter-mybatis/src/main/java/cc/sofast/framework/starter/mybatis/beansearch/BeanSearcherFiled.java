a403eabac3223d350ec7f154fabc172bc3d57a50
Administrator <Administrator@qq.com>
2025-06-19 22:07:38 +0800

    feat(starter-mybatis): 实现 BeanSearcherFiled 的条件映射功能
    - 新增 CLASS_FUNCTION_MAP 用于存储不同条件类型对应的处理函数
    - 初始化条件类型和处理函数的映射关系
    - 修改 getVal 方法，根据条件类型调用相应的处理函数
    - 优化了对不同查询条件的处理逻辑

--- sofast-spring-boot-starter/sofast-spring-boot-starter-mybatis/src/main/java/cc/sofast/framework/starter/mybatis/beansearch/BeanSearcherFiled.java
+++ sofast-spring-boot-starter/sofast-spring-boot-starter-mybatis/src/main/java/cc/sofast/framework/starter/mybatis/beansearch/BeanSearcherFiled.java
@@ -1,16 +1,40 @@
 package cc.sofast.framework.starter.mybatis.beansearch;
 
 import cn.zhxu.bs.FieldOp;
+import cn.zhxu.bs.operator.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 /**
  * @author wxl
  */
 public record BeanSearcherFiled(Condition condition, String filedName, Object filedVal) {
 
+    private static final Map<Class<? extends FieldOp>, Function<Object, Object>> CLASS_FUNCTION_MAP = new HashMap<>();
+
+    static {
+        //TODO 待完善
+        CLASS_FUNCTION_MAP.put(Equal.class, f -> f);
+        CLASS_FUNCTION_MAP.put(NotEqual.class, f -> f);
+        CLASS_FUNCTION_MAP.put(LessThan.class, f -> f);
+        CLASS_FUNCTION_MAP.put(LessEqual.class, f -> f);
+        CLASS_FUNCTION_MAP.put(GreaterThan.class, f -> f);
+        CLASS_FUNCTION_MAP.put(GreaterEqual.class, f -> f);
+        CLASS_FUNCTION_MAP.put(Contain.class, f -> f);
+        CLASS_FUNCTION_MAP.put(StartWith.class, f -> f);
+        CLASS_FUNCTION_MAP.put(EndWith.class, f -> f);
+        CLASS_FUNCTION_MAP.put(InList.class, f -> f);
+        CLASS_FUNCTION_MAP.put(Between.class, f -> f);
+    }
+
     public Object getVal() {
         Class<? extends FieldOp> clazz = condition.getClazz();
-        //TODO 根据类型转换
-
-        return filedVal;
+        Function<Object, Object> objectObjectFunction = CLASS_FUNCTION_MAP.get(clazz);
+        if (objectObjectFunction != null) {
+            return objectObjectFunction.apply(filedVal);
+        }
+        throw new RuntimeException("不支持的查询条件");
     }
 }
